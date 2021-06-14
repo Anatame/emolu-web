ef1e6b31c5dd5de506018ae43901c1a4f20edb86
Anatame <anata.me97@gmail.com>
2021-06-14 19:10:27 +0530

    Updated navbar css to react css objects

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,15 @@
 // eslint-disable-next-line
 import logo from '../../images/emolu.png'
 import Vector from '../../images/Vector.svg'
+import styles from '../../utils/styles'
 
 
 
 const Navbar = () => {
     return (
-        <nav className="navbar">
-            <img src={logo} alt="" />
-            <img src={Vector} alt="pfp" />
+        <nav style={styles.navbar}>
+            <img style={styles.navLogo} src={logo} alt="" />
+            <img style={styles.navAccountIcon} src={Vector} alt="pfp" />
 
         </nav>
     );
---
+++ src/utils/styles.js
@@ -0,0 +1,20 @@
+export default {
+   navbar: {
+      padding: 20,
+      display: 'flex',
+      alignItems: 'center',
+      margin: 'auto',
+      borderBottom: '1px solid #f2f2f2',
+      justifyContent: 'space-between',
+   },
+   
+   navLogo: {
+      height: '20px',
+      width: '90px',
+   },
+
+   navAccountIcon: {
+      height: '20px',
+      width: '90px',
+   }
+}
